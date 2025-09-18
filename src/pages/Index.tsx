09fb5a301493d97c83aeb440d7ef280aa1122de8
darylperisCM <darylperis.cm@gmail.com>
2025-09-19 00:07:28 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -66,7 +66,7 @@ export default function Index() {
     </div>
 
     {/* Hero Video */}
-    <div className="relative max-w-full mx-auto">
+    <div className="relative w-full mx-auto border-4 border-red-500">
       <HeroVideo />
     </div>
   </div>
